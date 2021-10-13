6515a58b77d7bef3a6373ff19a2d37064a396011
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-10-13 16:53:04 +0300

    about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -8,61 +8,66 @@ $this->title = 'Про нас';
 ?>
 <div class="container">
     <div class="row info info-header">
-        <div class="col-8">
+        <div class="col-sm-8">
             <h1 class="info-header-title">Команда</h1>
             <span class="info-header-text">Ми команда кроссекторального соціального проєкту “На рівних”.Познайомимось?</span>
         </div>
-        <div class="col-3">
+        <div class="col-sm-3">
             <img class="info-header-image" src="/images/about-smile.png" alt="">
         </div>
     </div>
-    <div class="row info row-cols-3">
-        <div class="col info-square">
+    <div class="row info">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #FDD749" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Злата Єфіменко</div>
             <div class="info-square-people-describe-text">керівниця проєкту, продюсерка</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #509FC8" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Софія Лавренюк</div>
             <div class="info-square-people-describe-text">керівниця проєкту, соціологиня</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #C4DE95" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Анастасія Левицька</div>
             <div class="info-square-people-describe-text">дизайнерка</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #F98DC0" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Віктор Коломієць</div>
             <div class="info-square-people-describe-text">дизайнер</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #C4DE95" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Євгенія Кужненкова</div>
             <div class="info-square-people-describe-text">смм-ниця</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #F98DC0" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Вікторія Одусанво</div>
             <div class="info-square-people-describe-text">соціологиня</div>
         </div>
-        <div class="col info-square"> <!-- Переделать костыль -->
+        <div class="col-xl-4 info-square"> <!-- Переделать костыль -->
             <div style= "padding-bottom: 362px ;background-image: url('https://onequals.com.ua/images/photos/about-us-1.png')" class="info-square-text"></div>
             <div class="info-square-people-text">Марія Деркачова</div>
             <div class="info-square-people-describe-text">режисерка</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #509FC8" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Антон Коломієць</div>
             <div class="info-square-people-describe-text">оператор</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #F98DC0" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Денис Максюра</div>
             <div class="info-square-people-describe-text">розробник сайту</div>
         </div>
-        <div class="col info-square">
+        <div class="col-xl-4 info-square">
+            <div style= "background-color: #FDD749" class="info-square-text">Тут буде красиве фото</div>
+            <div class="info-square-people-text">Станіслав Фесюк</div>
+            <div class="info-square-people-describe-text">розробник сайту</div>
+        </div>
+        <div class="col-xl-4 info-square">
             <div style= "background-color: #C4DE95" class="info-square-text">Тут буде красиве фото</div>
             <div class="info-square-people-text">Роман Єфіменко</div>
             <div class="info-square-people-describe-text">тестувальник</div>
@@ -75,15 +80,15 @@ $this->title = 'Про нас';
     <div class="steps-header-text">Слоган проєкту: “Тільки твій талант робить тебе особливим”. Нашою метою є руйнування стереотипів щодо працевлаштування людей з інвалідністю та надання інструменту для досягнення ними своїх професійних амбіцій.</div>
     </div>
     <div class="container">
-        <div class="row row-cols-2 justify-content-center">
-            <div class="col-3"><img src="/images/about-1.png" alt=""></div>
-            <div class="col-9 steps-text">Соціологічне дослідження, що слугувало розвідкою щодо ролі корпоративної культури у створенні сприятливих умов для працевлаштування людей з інвалідністю. Скоро ви зможете побачити результати у відкритому доступі.</div>
-            <div class="col-3"><img src="/images/about-2.png" alt=""></div>
-            <div class="col-9 steps-text">Платформа з пошуку вакансій. Ми намагалися створити сайт, який був би зрозумілим та простим у використанні, доступним для кожного та кожної. Він буде постійно оновлюватися корисною інформацією для шукачів_ок та роботодавців_чинь, а також вакансіями, які передбачають надання робочих місць для людей з інвалідністю. Сподіваємося на підтримку зі сторони компаній та розширення кола професійних можливостей для шукачів_ок.</div>
-            <div class="col-3"><img src="/images/about-3.png" alt=""></div>
-            <div class="col-9 steps-text">Серія соціальних відеороликів. Ролики висвітлюватимуть круті кейси працевлаштування людей з різним типом інвалідності у різних сферах ринку праці. Вони виступатимуть мотивацією для тих, хто зневірився у можливості знайти місце роботи за своїми професійними якостями або ж зіштовхнувся_лася з негативним досвідом працевлаштування.</div>
-            <div class="col-3"><img src="/images/about-4.png" alt=""></div>
-            <div class="col-9 steps-text">Документальний короткометражний фільм про буденність та історії працевлаштування людей з інвалідністю. Цим фільмом ми хочемо показати захопливі історії 5 різних героїв та героїнь: їх незвичайні хобі, цікаву роботу, друзів, родину.</div>
+        <div class="row">
+            <div class="col-xl-3"><img src="/images/about-1.png" alt=""></div>
+            <div class="col-xl-9 steps-text">Соціологічне дослідження, що слугувало розвідкою щодо ролі корпоративної культури у створенні сприятливих умов для працевлаштування людей з інвалідністю. Скоро ви зможете побачити результати у відкритому доступі.</div>
+            <div class="col-xl-3"><img src="/images/about-2.png" alt=""></div>
+            <div class="col-xl-9 steps-text">Платформа з пошуку вакансій. Ми намагалися створити сайт, який був би зрозумілим та простим у використанні, доступним для кожного та кожної. Він буде постійно оновлюватися корисною інформацією для шукачів_ок та роботодавців_чинь, а також вакансіями, які передбачають надання робочих місць для людей з інвалідністю. Сподіваємося на підтримку зі сторони компаній та розширення кола професійних можливостей для шукачів_ок.</div>
+            <div class="col-xl-3"><img src="/images/about-3.png" alt=""></div>
+            <div class="col-xl-9 steps-text">Серія соціальних відеороликів. Ролики висвітлюватимуть круті кейси працевлаштування людей з різним типом інвалідності у різних сферах ринку праці. Вони виступатимуть мотивацією для тих, хто зневірився у можливості знайти місце роботи за своїми професійними якостями або ж зіштовхнувся_лася з негативним досвідом працевлаштування.</div>
+            <div class="col-xl-3"><img src="/images/about-4.png" alt=""></div>
+            <div class="col-xl-9 steps-text">Документальний короткометражний фільм про буденність та історії працевлаштування людей з інвалідністю. Цим фільмом ми хочемо показати захопливі історії 5 різних героїв та героїнь: їх незвичайні хобі, цікаву роботу, друзів, родину.</div>
         </div>
     </div>
 </div>
