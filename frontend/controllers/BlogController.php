130f325e68ff1fc71867094a9fe0314e447f3acf
Rozrabiaka <denmaksiura@gmail.com>
2021-10-14 12:22:24 +0300

    updates

---
+++ frontend/controllers/BlogController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Site controller
+ */
+class BlogController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+            'auth' => [
+                'class' => 'yii\authclient\AuthAction',
+                'successCallback' => [$this, 'onAuthSuccess'],
+            ],
+        ];
+    }
+
+    public function actionSummary()
+    {
+        return $this->render('summary');
+    }
+
+    public function actionEmployer()
+    {
+        return $this->render('employer');
+    }
+
+    public function actionLegislation()
+    {
+        return $this->render('legislation');
+    }
+}
