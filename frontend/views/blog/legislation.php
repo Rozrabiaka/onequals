db282804fd53f979d076dc423dcc6e6384674340
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-10-14 15:40:18 +0300

    blog pages

--- frontend/views/blog/legislation.php
+++ frontend/views/blog/legislation.php
@@ -5,13 +5,13 @@
 <div class="blog-header" style="background-color: #F98DC0">
     <div class="container">
         <div class="row">
-            <div class="col-xl-4">
+            <div class="col-md-4">
                 <div class="blog-header-text">
                 <h1 class="info-header-title">Журнал</h1>
                 <div class="info-header-text">Законодавство</div>
                 </div>
             </div>
-            <div class="col-xl-8">
+            <div class="col-md-8">
                 <img class ="blog-image blog-legislation-pic" src="/images/blog-legislation.png" alt="">
                 <img class ="blog-image blog-legislation-star" src="/images/blog-star-legislation.png" alt="">
             </div>
