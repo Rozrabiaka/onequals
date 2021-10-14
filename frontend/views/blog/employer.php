3a47843efd78c45e3ad609bdb8710de5c6e8b691
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-10-14 14:29:44 +0300

    blog pages

--- frontend/views/blog/employer.php
+++ frontend/views/blog/employer.php
@@ -1,1 +1,20 @@
 <?php
+
+?>
+
+<div class="blog-header" style="background-color: #FDD749">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-8 blog-header-text">
+                <h1 class="info-header-title">Журнал</h1>
+                <div class="info-header-text">Роботодавцям</div>
+            </div>
+            <div class="col-xl-2">
+                <img class ="blog-image" style="position: absolute;-webkit-transform: rotate(
+18.11deg);top: calc(304px - 15.4vw);right: calc(-307px + 9.4vw);width: calc(330px + 29.1vw);" src="/images/blog-employer.png" alt="">
+            </div>
+            <div class="col-xl-2">
+                <img class ="blog-image" style="position:absolute; top: 98px;right: 730px;width: calc(75px + 0.7vw);" src="/images/blog-stars.png" alt="">
+            </div>
+    </div>
+</div>
