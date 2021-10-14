48e2951fcee296801b29bb958ef37a05d7e38605
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-10-14 14:53:50 +0300

    blog pages

--- frontend/views/blog/employer.php
+++ frontend/views/blog/employer.php
@@ -5,16 +5,19 @@
 <div class="blog-header" style="background-color: #FDD749">
     <div class="container">
         <div class="row">
-            <div class="col-xl-8 blog-header-text">
+            <div class="col-xl-4">
+                <div class="blog-header-text">
                 <h1 class="info-header-title">Журнал</h1>
                 <div class="info-header-text">Роботодавцям</div>
+                </div>
             </div>
-            <div class="col-xl-2">
+            <div class="col-xl-6">
                 <img class ="blog-image" style="position: absolute;-webkit-transform: rotate(
-18.11deg);top: calc(304px - 15.4vw);right: calc(-307px + 9.4vw);width: calc(330px + 29.1vw);" src="/images/blog-employer.png" alt="">
+18.11deg);top: calc(304px - 15.4vw);right: calc(-307px + 9.4vw);width: 100%" src="/images/blog-employer.png" alt="">
             </div>
             <div class="col-xl-2">
-                <img class ="blog-image" style="position:absolute; top: 98px;right: 730px;width: calc(75px + 0.7vw);" src="/images/blog-stars.png" alt="">
+                <img style="position:absolute; top: 98px;right: 690px;width: calc(75px + 0.7vw);" src="/images/blog-stars.png" alt="">
             </div>
     </div>
+</div>
 </div>
--- frontend/views/blog/legislation.php
+++ frontend/views/blog/legislation.php
@@ -5,16 +5,18 @@
 <div class="blog-header" style="background-color: #F98DC0">
     <div class="container">
         <div class="row">
-            <div class="col-xl-8 blog-header-text">
+            <div class="col-xl-4">
+                <div class="blog-header-text">
                 <h1 class="info-header-title">Журнал</h1>
                 <div class="info-header-text">Законодавство</div>
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <img class ="blog-image" style="position: absolute;top: calc(304px - 15.4vw);right: calc(-307px + 9.4vw);width: 100%;" src="/images/blog-legislation.png" alt="">
             </div>
             <div class="col-xl-2">
-                <img class ="blog-image" style="position: absolute;-webkit-transform: rotate(
-18.11deg);top: calc(304px - 15.4vw);right: calc(-307px + 9.4vw);width: calc(330px + 29.1vw);" src="/images/blog-legislation.png" alt="">
-            </div>
-            <div class="col-xl-2">
-                <img class ="blog-image" style="position:absolute; top: 98px;right: 600px;width: calc(75px + 0.7vw);" src="/images/blog-stars.png" alt="">
+                <img class ="blog-image" style="position:absolute; top: 98px;right: 600px;width: calc(75px + 0.7vw);" src="/images/blog-star-legislation.png" alt="">
             </div>
         </div>
     </div>
+</div>
